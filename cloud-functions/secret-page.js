3905c14db6426309ced1e2cb31d724bc8792e437
Ganzorig <email@address.com>
2020-03-08 16:00:07 +0800

    cloud function-iig update hiilee

--- cloud-functions/secret-page.js
+++ cloud-functions/secret-page.js
@@ -1,6 +1,25 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Хэрэглэгчдийн хуудсан тавтай морилно уу"
-  });
+  const secretContent = `
+  <h3>Хэрэглэгчийн тусгай хуудсанд тавтай морилно уу!</h3>
+  <p>Зөвхөн бүртгэлтэй хэрэглэгчдэд зориулсан <strong>хаалттай контентууд.</strong></p>
+  `;
+
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password == "JavaScript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    });
+  } else {
+    callback(null, {
+      statusCode: 401
+    });
+  }
 };
